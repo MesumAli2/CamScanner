8b8fc0a9851a145159854d49ce0a33aa0922bff4
MesumAli2 <smesumali20@gmail.com>
2023-05-01 22:09:25 +0400

    text recogniton api implemented

--- app/src/main/java/com/mesum/camscanner/MainActivity.kt
+++ app/src/main/java/com/mesum/camscanner/MainActivity.kt
@@ -5,12 +5,18 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
@@ -20,11 +26,15 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.lifecycle.LifecycleOwner
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.text.TextRecognizer
 import com.itextpdf.text.Document
 import com.itextpdf.text.Image
+import com.itextpdf.text.Paragraph
 import com.itextpdf.text.pdf.PdfWriter
 import java.io.File
 import java.io.FileOutputStream
+import java.io.FilterOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -82,12 +92,10 @@ class MainActivity : AppCompatActivity() {
                         CAMREA_REQUEST_CODE
                     )
                     createPdfFromImage(photoFile!!)
-                    // To load the PDF using PDFView
-                    val imageFile = File("path/to/imageFile.jpg")
-//                    val pdfFile = createPdfFromImage(imageFile)
-//
-//                    val pdfView = findViewById<PDFView>(R.id.pdfView)
-//                    pdfView.fromFile(pdfFile).load()
+
+                    Log.d("PdfFile", photoFile.toString())
+
+
                 }
             } catch (ex: java.lang.Exception) {
                 // Error occurred while creating the File
@@ -163,11 +171,94 @@ class MainActivity : AppCompatActivity() {
                 if (resultCode == Activity.RESULT_OK) {
                     val bundle = data?.getStringExtra("image_path")
                     val bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
+                    findViewById<ImageView>(R.id.imageView).setImageBitmap(bitmap)
+                    recognizeText(bitmap)
+                    Log.d("ImageData","bitmap : ${bitmap.toString()} bundle : ${bundle.toString()}"  )
                 }
+
             }
 
 
         }
     }
+
+
+    private fun recognizeText(bitmap: Bitmap) {
+        val textRecognizer = TextRecognizer.Builder(this).build()
+
+        if (!textRecognizer.isOperational) {
+            // Check if the text recognition API is available
+            Toast.makeText(this, "Text recognition is not available.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Create a Frame object from the bitmap
+        val frame = Frame.Builder().setBitmap(bitmap).build()
+
+        // Use the text recognition API to recognize text from the image
+        val textBlocks = textRecognizer.detect(frame)
+
+        // Extract the text from each TextBlock and concatenate them into a single string
+        val text = StringBuilder()
+        for (i in 0 until textBlocks.size()) {
+            val textBlock = textBlocks.valueAt(i)
+            text.append(textBlock.getValue())
+        }
+
+        // Create a PDF file from the recognized text
+        Log.d("TextTest", text.toString())
+        createPdfFromText(text.toString())
+    }
+
+
+
+    private fun createPdfFromText(text: String) {
+        // Create a new file for the PDF
+        val pdfFile = File(this.filesDir, "text.pdf")
+
+        // Create a PDF document
+        val document = PdfDocument()
+
+        // Create a page for the document
+        val pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
+        val page = document.startPage(pageInfo)
+
+        // Create a canvas to draw the text on the page
+        val canvas = page.canvas
+        val paint = Paint()
+        paint.textSize = 12f
+        paint.color = Color.BLACK
+
+        // Split the text into lines
+        val lines = text.split("\n")
+
+        // Draw each line of text on the canvas
+        var y = 0f
+        for (line in lines) {
+            canvas.drawText(line, 0f, y, paint)
+            y += paint.descent() - paint.ascent()
+        }
+
+        // Finish the page and add it to the document
+        document.finishPage(page)
+
+        // Write the document to the PDF file
+        val outputStream = FileOutputStream(pdfFile)
+        document.writeTo(outputStream)
+
+        // Close the document and the output stream
+        document.close()
+        outputStream.close()
+
+        // Share the PDF file with other apps using a FileProvider
+        val uri = FileProvider.getUriForFile(this, "com.mesum.camscanner.fileprovider", pdfFile)
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setDataAndType(uri, "application/pdf")
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        val chooserIntent = Intent.createChooser(intent, "Open PDF")
+        chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(chooserIntent)
+    }
+
 
 }
